8a041d6688934eb750ada3ef0520a3d5b85ef982
muneebulislam <muneebulislam@hotmail.com>
2023-01-20 21:08:53 -0600

    deletMany() application

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/fruitsDb');
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   const fruitSchema = new mongoose.Schema({
-    name: String,
+    name: {
+      type:String, 
+      required:[true, "Name of the fruit is required."],
+    },
     rating: Number,
     review: String
   });
@@ -20,11 +23,37 @@ async function main() {
   //inside the parenthesis 'Fruit' is name of collection that will be automatically converted to Fruits name.
   const mango = new Fruit({ name: 'Mango', rating: 8, review: 'Yellow and very sweet!'});
   console.log(mango.name);
+  // we can insert many fruits to collection as follows
+  const apple = new Fruit({
+    name: "apple",
+    rating: 5,
+    review:"An apple a day keeps the doctor away!"
+  });
+
+  const kiwi = new Fruit({
+    name: "kiwi",
+    rating: 5,
+    review: "kiwi is sour!"
+  });
+
+  const orange = new Fruit({
+    name: "orange",
+    rating: 3,
+    review: "refreshing!"
+  });
+
+  Fruit.insertMany([orange,kiwi,apple], (err)=>{
+    if(err){console.log(err)
+    } else {
+      console.log("All the fruits inserted in the Fruits collection!")
+    }
+  })
+
   // Now save mango to the mongo database.
-//   await mango.save();
+  // await mango.save();
   // Now we can find everything about the Fruit collection
-  const fruits = await Fruit.find();
-  console.log("Fruits collection is given by \n"+fruits)
+  // const fruits = await Fruit.find();
+  // console.log("Fruits collection is given by \n"+fruits)
 
   //Another collection.
   const personSchema = mongoose.Schema({
@@ -34,19 +63,23 @@ async function main() {
 
   const Person  = mongoose.model('Person', personSchema);
 //   const john = new Person({name:"John", age: 32});
-const kohn = new Person({name: "Kohn", age:150});
-await kohn.save((err)=>{
-    if(err){console.log("Person does not meet the data entry requirements!")}
-    else {
-        console.log("document successfully entered into the Person collection.")
-    }
-})
 //   await john.save();
-  const people = await Person.find((err,data)=>{
+
+// Delete all the documents in Fruits model where name is apple
+// Fruit.deleteMany({ name: 'apple' }, function (err) {
+//   if (err) {console.log(err)};
+//   // deleted at most one Fruit document
+// });
+
+
+ Fruit.find((err,data)=>{ // here data is an array of objects. 
     if(err){
         console.log(err);
     } else {
-        console.log("Person collection is given by:\n"+data)
+      mongoose.connection.close();
+        data.forEach((item)=>{
+          console.log(item.name);
+        })
     }
   });
 
